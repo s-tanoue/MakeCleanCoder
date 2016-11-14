d4a8f3c2f0789c25ce4c62252a528645ca52b10c
SatoshiTanoue <again2222@gmail.com>
2016-11-14 19:17:09 +0900

    ハイパーリンクで解析結果を表示できるようにした

--- src/makecleancoder/Controller.java
+++ src/makecleancoder/Controller.java
@@ -2,15 +2,21 @@ package makecleancoder;
 
 import CommentParser.CleanCoderCommentParser;
 import Dictionaly.CommentDictionaly;
-import GetTextAreaNumber.CleanCoderTextArea;
 import Parser.CleanCoderParser;
 import Parser.ParseException;
 import ResultData.ResultData;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.text.TextFlow;
+import javafx.scene.web.WebView;
 import javafx.stage.FileChooser;
 
 import java.io.BufferedReader;
@@ -34,16 +40,23 @@ public class Controller implements Initializable {
   @FXML
   private AnchorPane root;
   @FXML
-  private CleanCoderTextArea editArea;
+  private TextArea editArea;
   @FXML
-  private CleanCoderTextArea consoleArea;
+  private TextFlow consoleArea;
   @FXML
-  private CleanCoderTextArea lineNumberArea;
+  private VBox consoleAreaVbox;
+  @FXML
+  private TextArea lineNumberArea;
   private String crlf = System.getProperty("line.separator");
   
+  
+  @FXML 
+  private void hanleHyperlinkAction(ActionEvent event){
+    System.out.println("Hello");
+  }
+
   @FXML
   private void fileOpen(ActionEvent event) {
-    
     FileChooser fileChooser = new FileChooser();
     fileChooser.setTitle("Open Resource File");
     File selectedFile = fileChooser.showOpenDialog(root.getScene().getWindow());
@@ -57,7 +70,7 @@ public class Controller implements Initializable {
         }
       }
       editArea.setText(inputAllString);
-      int lineNumber = editArea.getLineNumber(editArea.getText());
+      int lineNumber = inputAllString.split(crlf,-1).length; 
       String line = "";
       for (int i = 1; i <= lineNumber; i++) {
         line += String.valueOf(i) + crlf;
@@ -85,10 +98,10 @@ public class Controller implements Initializable {
 
   // 戻り値 コンソールエリアに出力する文字列
   // 引数 解析するソースコード
-  private String commentParse(String inputString)
+  private ArrayList<Hyperlink> commentParse(String inputString)
   {
-	  String outPutString = "";
 	  ResultData result= new ResultData(inputString);
+	  ArrayList<Hyperlink> outPutLink= new ArrayList<Hyperlink>();
 
 	  for(int i = 0; i < result.map.size();  i++) {
 		  ArrayList<String> comment = result.map.get(result.keyValue.get(i));
@@ -96,103 +109,104 @@ public class Controller implements Initializable {
 			  CommentDictionaly dictionaly = new CommentDictionaly();
 			  //適切なコメントかどうか判断する．
 			  if (!dictionaly.isRequiredComment(comment.get(j))) {
-				  outPutString += String.valueOf(result.keyValue.get(i)) + ":"
-						  +comment.get(j).replaceAll(crlf, "") + " は不要なコメントです\n";
+				  outPutLink.add(new Hyperlink(String.valueOf(result.keyValue.get(i)) + ":" +comment.get(j).replaceAll(crlf, "") + " は不要なコメントです"+crlf));
 			  }
 		  }
 	  }
-	  return outPutString;
+	  return outPutLink;
   }
   @FXML
-  //複数ファイルのコメントを解析するコメント
+  //ファイルのコメントを解析する
   private void executeParseComment(ActionEvent event) {
 
-	  String editAreaText = editArea.getText();
-	  String  outPutString = commentParse(editAreaText);
-	  consoleArea.setText(outPutString);
+	 String editAreaText = editArea.getText();
+	  ArrayList<Hyperlink> outPutLink = commentParse(editAreaText);
+
+    consoleArea.getChildren().clear();
+	  for(Hyperlink link : outPutLink){
+	    consoleArea.getChildren().add(link);
+	  }
   }
 
   //複数ファイルのコメントを解析
   @FXML
   private void executeParseMultipleFileOfComment(ActionEvent event) {
 
-	  FileChooser fileChooser = new FileChooser();
-	  fileChooser.setTitle("Open Resource File");
-	  List<File> selectedFile = fileChooser.showOpenMultipleDialog(root.getScene().getWindow());
-	  String inputString="";
-	  String outputString="";
-	  BufferedReader br;
-	  try {
-		  for(File file: selectedFile){
-			  br = new BufferedReader(new FileReader(file));
-			  String str;
-			  while ((str = br.readLine()) != null) {
-				  inputString += str + crlf;
-			  }
-			 outputString += "ファイル名:"+file.getName()+"\n"+commentParse(inputString)+crlf;
-		  }
-	  } catch (IOException e) {
-		  e.printStackTrace();
-	  }
-	  consoleArea.setText(outputString);
-
+    FileChooser fileChooser = new FileChooser();
+    fileChooser.setTitle("Open Resource File");
+    List<File> selectedFile = fileChooser.showOpenMultipleDialog(root.getScene().getWindow());
+    String inputString="";
+    String outputString="";
+    BufferedReader br;
+    try {
+      for(File file: selectedFile){
+        br = new BufferedReader(new FileReader(file));
+        String str;
+        while ((str = br.readLine()) != null) {
+          inputString += str + crlf;
+        }
+        outputString += "ファイル名:"+file.getName()+"\n"+commentParse(inputString)+crlf;
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    //  consoleArea.setText(outputString);
   }
 
   //変数名を解析
   @FXML
   private void executeParser(ActionEvent event) {
-	  String str = editArea.getText();
-	  String[] strs = str.split(crlf);
-	  String outPutString = "";
-	  List<String> result = new ArrayList<String>();
-	  int count = 1;
-	  for (int i = 0; i < strs.length; i++) {
-		  try {
-			  CleanCoderParser parser = new CleanCoderParser(new StringReader(strs[i]));
-			  result = parser.VariableDeclaration();
-			  for (int j = 0; j < result.size(); j++) {
-				  if (!result.get(j).isEmpty()) {
-					  outPutString += String.valueOf(count) + ":" + result.get(j) + crlf;
-				  }
-			  }
-		  } catch (ParseException e) {
-			  e.printStackTrace();
-		  }
-		  count++;
-	  }
-	  consoleArea.setText(outPutString);
+    String str = editArea.getText();
+    String[] strs = str.split(crlf);
+    String outPutString = "";
+    List<String> result = new ArrayList<String>();
+    int count = 1;
+    for (int i = 0; i < strs.length; i++) {
+      try {
+        CleanCoderParser parser = new CleanCoderParser(new StringReader(strs[i]));
+        result = parser.VariableDeclaration();
+        for (int j = 0; j < result.size(); j++) {
+          if (!result.get(j).isEmpty()) {
+            outPutString += String.valueOf(count) + ":" + result.get(j) + crlf;
+          }
+        }
+      } catch (ParseException e) {
+        e.printStackTrace();
+      }
+      count++;
+    }
+    // consoleArea.setText(outPutString);
 
   }
 
   @FXML
   private void createLineNumber(KeyEvent event) {
-	  if ("ENTER".equals(event.getCode().toString())) {
-		  double scrollTop = editArea.getScrollTop();
-		  String string = editArea.getText();
-		  int lineNumber = editArea.getLineNumber(string);
-		  String line = "";
-		  for (int i = 1; i <= lineNumber + 1; i++) {
-			  line += String.valueOf(i) + crlf;
-		  }
-		  lineNumberArea.setText(line);
-		  editArea.setScrollTop(scrollTop);
-	  }
+    if ("ENTER".equals(event.getCode().toString())) {
+      double scrollTop = editArea.getScrollTop();
+      int lineNumber = editArea.getText().split(crlf,-1).length; 
+      String line = "";
+      for (int i = 1; i <= lineNumber + 1; i++) {
+        line += String.valueOf(i) + crlf;
+      }
+      lineNumberArea.setText(line);
+      editArea.setScrollTop(scrollTop);
+    }
   }
 
   @FXML
   private void clearConsoleArea(ActionEvent event) {
-	  consoleArea.setText("");
+    //  consoleArea.setText("");
   }
 
   @FXML
   private void clearEditArea(ActionEvent event) {
-	  editArea.setText("");
-	  lineNumberArea.setText("1"+crlf);
+    editArea.setText("");
+    lineNumberArea.setText("1"+crlf);
   }
 
   @Override
   public void initialize(URL url, ResourceBundle rb) {
-	  editArea.scrollTopProperty().bindBidirectional(lineNumberArea.scrollTopProperty());
+    editArea.scrollTopProperty().bindBidirectional(lineNumberArea.scrollTopProperty());
   }
 
 }
